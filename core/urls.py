107363b54e5b78263e212eb291d8278e5d43c073
dheeraj <dheerajpatidar20575@gmail.com>
2025-02-26 16:29:18 +0530

    added forgot password functionality

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('index/',views.index,name="index"),
@@ -8,6 +9,12 @@ urlpatterns = [
     path('login/',views.Login_page,name="login_page"),
     path('logout/',views.logout,name="logout"),
     path('student_dashboard/',views.Student_page,name="student_dashboard"),
-    path('student_profile/',views.Student_Profile,name="student_profile")
+    path('student_profile/',views.Student_Profile,name="student_profile"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
+
+
 
 ]
